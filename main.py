540be1aaa0eb1869c3a629125aec9e1a16d353ff
nanodino <nancycantusal@gmail.com>
2022-07-29 15:31:17 -0400

    initialize imports, basic structure

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+import time
+import pandas as pd
+
+def main():
+    print("main!")
+
+if __name__ == '__main__':
+    main()

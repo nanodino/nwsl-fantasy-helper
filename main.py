2c9a1d5ad8a307ac7d574c313cc787ec92ebfd4d
nanodino <nancycantusal@gmail.com>
2022-07-29 15:40:57 -0400

    load fotmob match page (test url)

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from selenium.webdriver.firefox.options import Options
 import time
 import pandas as pd
 
+def access_fotmob():
+    options = Options()
+    options.add_argument("-headless")
+    driver = Firefox(options = options)
+
+    match_url = "https://www.fotmob.com/match/3846084/matchfacts/portland-thorns-vs-houston-dash"
+    match_page = driver.get(match_url)
+    time.sleep(3)
+    match_source = driver.page_source
+
+    return match_source
+
 def main():
     print("main!")
 

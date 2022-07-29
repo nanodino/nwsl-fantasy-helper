aaa271151e48d69104e5abeb32f82d6fb9a8f470
nanodino <nancycantusal@gmail.com>
2022-07-29 16:00:45 -0400

    added read_match_data, calls

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 import time
 import pandas as pd
+import re
 
 def access_fotmob():
     options = Options()
@@ -16,8 +17,16 @@ def access_fotmob():
 
     return match_source
 
+
+def read_match_data(match_source):
+    match_soup = BeautifulSoup(match_source)
+    print(match_soup)
+
+
 def main():
     print("main!")
+    match_source = access_fotmob()
+    read_match_data(match_source)
 
 if __name__ == '__main__':
     main()

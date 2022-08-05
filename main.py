05cb421edeea18bad27b303dfb4d73fae0bcd56b
nanodino <nancycantusal@gmail.com>
2022-08-05 09:39:41 -0400

    refactor webdriver out of function

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ import re
 import pathlib
 import os
 
-def access_fbref():
+def set_up_webdriver():
     options = Options()
     options.add_argument("-headless")
     serv = Service(log_path=os.devnull, executable_path = GeckoDriverManager().install())
     driver = Firefox(options=options, service=serv)
 
-    match_url = "https://fbref.com/en/matches/7f334b6a/Houston-Dash-Portland-Thorns-FC-June-12-2022-NWSL"
+    return driver
+
+def access_fbref(driver, match_url):
     match_page = driver.get(match_url)
     time.sleep(9)
     match_source = driver.page_source
@@ -30,7 +32,8 @@ def read_match_data(match_source):
 
 def main():
     print("main!")
-    match_source = access_fbref()
+    driver = set_up_webdriver()
+    match_source = access_fbref(driver)
     read_match_data(match_source)
 
 if __name__ == '__main__':

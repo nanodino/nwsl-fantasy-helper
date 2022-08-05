06998e3d21107913ec5ad9bdf7421739487adeaa
nanodino <nancycantusal@gmail.com>
2022-08-05 10:21:47 -0400

    rename scores and fixtures function

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def set_up_webdriver():
 
     return driver
 
-def get_list_of_match_report_urls(driver):
+def get_scores_and_fixtures_table(driver):
     scores_and_fixtures_url = "https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures#sched_11498_1"
     scores_and_fixtures_page = driver.get(scores_and_fixtures_url)
     time.sleep(3)
@@ -25,7 +25,7 @@ def get_list_of_match_report_urls(driver):
     scores_soup = BeautifulSoup(scores_and_fixtures_source, features="html.parser")
     scores_table = scores_soup.find("table", {"id": "sched_11498_1"})
 
-    print(scores_and_fixtures_source)
+    return scores_table
 
     return match_source
 
@@ -38,8 +38,8 @@ def read_match_data(match_source):
 def main():
     print("main!")
     driver = set_up_webdriver()
-    match_source = access_fbref(driver)
-    read_match_data(match_source)
+    scores_and_fixtures = get_scores_and_fixtures_table(driver)
+    # match_source = access_fbref(driver)
 
 if __name__ == '__main__':
     main()
